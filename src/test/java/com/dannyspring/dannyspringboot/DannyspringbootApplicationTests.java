2d8971e50d61757ce5e08e2de3f1dabc0c3a4b6d
Danny <dangeo36@gmail.com>
2023-09-07 03:34:29 -0500

    added Student class

---
+++ src/test/java/com/dannyspring/dannyspringboot/DannyspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dannyspring.dannyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DannyspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
