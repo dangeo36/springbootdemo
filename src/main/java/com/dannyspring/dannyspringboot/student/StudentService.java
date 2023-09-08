26c2ace9df7a3aa5d6f75148ae4c1a102803943d
Danny <dangeo36@gmail.com>
2023-09-08 00:51:00 -0500

    added PUT method

--- src/main/java/com/dannyspring/dannyspringboot/student/StudentService.java
+++ src/main/java/com/dannyspring/dannyspringboot/student/StudentService.java
@@ -1,9 +1,11 @@
 package com.dannyspring.dannyspringboot.student;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -40,4 +42,30 @@ public class StudentService {
         studentRepository.deleteById(studentId);
 
     }
-}
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException(
+                        "student with id" + studentId + " does not exist"
+                ));
+                if (name != null &&
+                   name.length() > 0 &&
+                   !Objects.equals(student.getName(), name)) {
+                    student.setName(name);
+                }
+                if (email != null &&
+                    email.length() > 0 &&
+                    !Objects.equals(student.getEmail(), email)) {
+                    Optional<Student> studentOptional = studentRepository
+                            .findStudentByEmail(student.getEmail());
+                    if (studentOptional.isPresent()) {
+                        throw new IllegalStateException("email taken");
+                    }
+                }
+                    student.setEmail(email);
+    }
+
+
+    }
+
